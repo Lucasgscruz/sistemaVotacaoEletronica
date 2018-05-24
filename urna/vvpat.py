8f86ebee4eb40b20a05078a77c2322f912bd5775
exterminus <cmagnobarbosa@gmail.com>
2018-05-23 22:31:30 -0300

    ajustes no monitor de ambiente

--- urna/vvpat.py
+++ urna/vvpat.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# biblioteca
+# https://pypi.org/project/qrcode/
+
+"""
+Geração do QrCode
+"""
+
+
+def gera_qrcode():
+    """Gera o qrcode"""
+    pass
+
+
+def gera_vvpat():
+    """Combina o QrCode com info para gerar vvpat"""
+    pass
