b1e173201b95b334c89575bd870d2bab102bc300
Lucas <lucasgscruz10@hotmail.com>
2018-06-13 15:23:26 -0300

    contador corrigido

--- contador/contador.py
+++ contador/contador.py
@@ -18,6 +18,7 @@ def salvarVotos():
         if candidatos[i][4] == 'Prefeito':
             arq.write(candidatos[i][1] + ': ')
             arq.write(candidatos[i][5] + '\n')
+            
     arq.write("\nVereador:\n")
     for i in candidatos:
         if candidatos[i][4] == 'Vereador':
@@ -33,8 +34,6 @@ def contarVoto(vereador, prefeito):
     inicializaHash()
     candidatos[int(vereador)][5] += 1
     candidatos[int(prefeito)][5] += 1
-    for i in candidatos:
-        print candidatos[i][1]+ ": " + str(candidatos[i][5])
 
 def get_candidados(vetor):
     """Retorna o dado dos candidatos"""
