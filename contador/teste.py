cdbf5f6f8d38afb4d79e4566bdf287b5adc3064b
Lucas <lucasgscruz10@hotmail.com>
2018-06-13 15:16:02 -0300

    versao inicial contador

--- contador/teste.py
+++ contador/teste.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import eel
 import eleicao as eleicao
 import qr as qr_code
-eel.init('contador_gui')
+
 
 partidos = eleicao.carrega_partidos()
 candidatos = eleicao.carrega_candidatos()
-v_prefeito = {}
-v_vereador = {}
 
-@eel.expose
 def salvarVotos():
     arq = open("votos.txt", "a")
     arq.write("Prefeito:\n")
@@ -36,6 +32,7 @@ def contarVoto(vereador, prefeito):
     for i in candidatos:
         print candidatos[i][1]+ ": " + str(candidatos[i][5])
 
+
 def get_candidados(vetor):
     """Retorna o dado dos candidatos"""
     lista_retorno = []
@@ -43,19 +40,4 @@ def get_candidados(vetor):
         lista_retorno.append(candidatos[int(i)])
     return lista_retorno
 
-@eel.expose
-def carrega_final(escolhas):
-    """Realiza a pesquisa pelo numero e carrega os dados finais"""
-    candidatos = get_candidados(escolhas)
-    eel.tabela_final(candidatos)
-
-@eel.expose
-def verificar_vvpat():
-    """ verifica o conteudo do vvpat"""
-    codigo=qr_code.qr_cam()
-    vereador=codigo[0]
-    prefeito=codigo[1]
-    contarVoto(vereador,prefeito)
-    eel.redirecionar(vereador,prefeito)
-
-eel.start('index_contador.html')
+salvarVotos()
